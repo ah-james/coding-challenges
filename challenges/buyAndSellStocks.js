4d04c1c18bb40a693b49f1d3708d7602cd853b57
ah-james <11ahj11@gmail.com>
2022-01-11 16:47:25 -0500

    Buy and Sell Stocks Challenge

---
+++ challenges/buyAndSellStocks.js
@@ -0,0 +1,19 @@
+var maxProfit = function(prices) {
+    // save buyDate variable to update as we loop
+    let buyDate = prices[0]
+    // save profit variable to update when prices[i] - buyDate > current profit
+    let profit = 0
+    
+    // loop through prices array
+    for (let i = 1; i < prices.length; i++) {
+        // when i + 1 > buyDate, we want to update buyDate to prices[i]
+        if (prices[i] < buyDate) {
+            buyDate = prices[i]
+        } else if (prices[i] - buyDate > profit) {
+            // if prices[i] - buyDate is greater than profit, update that variable
+            profit = prices[i] - buyDate
+        }
+    }
+    
+    return profit
+};
