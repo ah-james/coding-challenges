e404110b74eabe5e063ba8ac44e0e81f2664f284
ah-james <11ahj11@gmail.com>
2023-02-02 12:31:16 -0500

    Clearing out old challenges for fresh start

--- challenges/mergeSortedArray.js
+++
@@ -1,7 +0,0 @@
-// take array of arrays and merge them into a single sorted array
-
-const mergeSortedArray = (arrays) => {
-    // chain methods, use flat() to make all arrays into single array
-    // use sort to sort array
-    return arrays.flat().sort((a, b) => a - b)
-}
