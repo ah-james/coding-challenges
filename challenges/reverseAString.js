b6be562d694c46d0d02fe0df05f53ed37d9742a8
ah-james <11ahj11@gmail.com>
2023-02-02 13:41:26 -0500

    reversed string function

---
+++ challenges/reverseAString.js
@@ -0,0 +1,21 @@
+const reverse = string => {
+    // add error handling
+    if (!string || typeof string !== 'string') {
+        return 'the entered value was not a string'
+    }
+    // take initial string and split into array of single characters
+    // save second array to join and return at end
+    let array = [...string]
+    const reversed = []
+  
+    // loop over array, take each element and push onto FRONT of reversed array (important)
+    for (let i = 0; i < array.length; i++) {
+      let char = array[i]
+      reversed.unshift(char)
+    }
+  
+    return reversed.join('')
+}
+
+// coward's way out (simpler way)
+const reverseTwo = string => [...string].reverse().join('')
