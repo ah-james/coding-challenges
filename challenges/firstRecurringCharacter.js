68c65d76e7f778a046a2088262a66bfd1a1795d3
ah-james <11ahj11@gmail.com>
2023-02-15 14:32:04 -0500

    firstRecurringCharacter

---
+++ challenges/firstRecurringCharacter.js
@@ -0,0 +1,17 @@
+// given an array, return the first repeating number
+// ex. [2,5,1,2,3,5,1,2,4] return 2
+// [2,1,1,2,3,5,1,2,4] return 1
+// [2,3,4,5] return undefined
+
+const firstRecurringCharacter = array => {
+    const obj = {} // set an empty object
+
+    for (let i = 0; i < array.length; i++) { // loop through array
+        if (obj[array[i]]) { // check if array[i] exists in the object
+            return obj[array[i]]
+        }
+        obj[array[i]] = 1 // if array[i] doesn't exist add it to the object
+    }
+}
+
+firstRecurringCharacter([2,5,1,2,3,5,1,2,4])
