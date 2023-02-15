711980b81f9ae84c94ea0979fcdec47a8d3c50c3
ah-james <11ahj11@gmail.com>
2023-02-15 13:13:29 -0500

    improving implementHashTable

--- challenges/implementHashTable.js
+++ challenges/implementHashTable.js
@@ -15,11 +15,25 @@ class HashTable {
     }
 
     set(key, value) {
-        this.data[this._hash(key)] = [key, value]
+        let address = this.hash(key)
+        if (!this.data[address]) { // check to see if something already exists in current memory space
+            this.data[address] = [] // if it doesn't create an empty array, this prevents data from being overwritten if something new is added to same memory space
+        }
+        this.data[address].push([key, value])
+        return this.data
     }
 
     get(key) {
-        console.log(this.data[this._hash(key)])
-        return this.data[this._hash(key)]
+        // return this.data[this._hash(key)] can't do this in case multiple things saved in this location in memory, also just want value, no key
+        let address = this._hash(key)
+        let currentSpace = this.data[address] // check if something exists at current location in memory
+        if (currentSpace) {
+            for (let i = 0; i < currentSpace.length; i++) { // loop through array in memory
+                if (currentSpace[i][0] === key) { // if the key is the same as the key saved in memory, return the value seen there
+                    return currentSpace[i][1]
+                }
+            }
+        }
+        return undefined
     }
 }
