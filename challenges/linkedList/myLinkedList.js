1da79f8e147a63c00db0c52c327619923e1e1e4f
ah-james <11ahj11@gmail.com>
2023-02-28 16:23:10 -0500

    drying up code - setting up Node class

--- challenges/linkedList/myLinkedList.js
+++ challenges/linkedList/myLinkedList.js
@@ -1,3 +1,10 @@
+class Node {
+    constructor(value) {
+        this.value = value,
+        this.next = null
+    }
+}
+
 class LinkedList {
     constructor(value) {
         this.head = {
@@ -10,11 +17,7 @@ class LinkedList {
 
     // construct append method
     append(value) {
-        // create a new node
-        const newNode = {
-            value: value,
-            next: null
-        }
+    const newNode = new Node(value) // create a new node
 
         this.tail.next = newNode // make the next value of previous tail into the new node  
         this.tail = newNode // now that this.tail.next is reset, this.tail can be changed to the new node
@@ -23,12 +26,9 @@ class LinkedList {
     }
 
     prepend(value) {
-        // create a new node
-        const newNode = {
-            value: value,
-            next: this.head // new node should have a next of the rest of the linked list
-        }
+        const newNode = new Node(value) // create a new node
 
+        newNode.next = this.head // new node should have a next of the rest of the linked list
         this.head = newNode
         this.length++
         return this
