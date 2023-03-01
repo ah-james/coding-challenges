de7744d61b22c1c4c9c38c372a65c56f7433a7a3
ah-james <11ahj11@gmail.com>
2023-03-01 14:25:12 -0500

    insert method in linked list

--- challenges/linkedList/myLinkedList.js
+++ challenges/linkedList/myLinkedList.js
@@ -1,7 +1,7 @@
 class Node {
     constructor(value) {
         this.value = value,
-        this.next = null
+            this.next = null
     }
 }
 
@@ -17,7 +17,7 @@ class LinkedList {
 
     // construct append method
     append(value) {
-    const newNode = new Node(value) // create a new node
+        const newNode = new Node(value) // create a new node
 
         this.tail.next = newNode // make the next value of previous tail into the new node  
         this.tail = newNode // now that this.tail.next is reset, this.tail can be changed to the new node
@@ -33,11 +33,53 @@ class LinkedList {
         this.length++
         return this
     }
+
+    printList() {
+        const array = []
+        let currentNode = this.head
+        while (currentNode !== null) {
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array
+    }
+
+    insert(index, value) {
+        if (index === 0) {
+            this.prepend(value);
+            return this.printList();
+        }
+
+        if (index >= this.length) { // if the index is higher than the length the value should just be appended
+            this.append(value)
+        }
+
+        const newNode = new Node(value)
+
+        const leader = this.traverseToIndex(index-1) // use helper method to find the node directly before where new node should be entered
+        const holdingPointer = leader.next // opposite side of where newNode should be inserted
+        leader.next = newNode // now that leader.next has been saved as a separate variable, newNode can be inserted and connected to leader
+        newNode.next = holdingPointer // to tie newNode to holdingPointer, just make newNode.next from null to holdingPointer
+        this.length++
+        return this.printList()
+    }
+
+    traverseToIndex(index) {
+        let counter = 0
+        let currentNode = this.head // start iteration at beginning
+        while (counter !== index) { // as long as the counter isn't the same as the index passed in
+            currentNode = currentNode.next // set the currentNode to the next ex. [1,10,5,16] for insert at i=2 currentNode starts as 1, then counter becomes 1 and currentNode becomes 10
+            counter++
+        }
+        return currentNode
+    }
 }
 
 const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
+myLinkedList.insert(2, 100)
+console.log(myLinkedList.printList())
 
 console.log(myLinkedList)
