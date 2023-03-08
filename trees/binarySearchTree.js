514d3b5e82be720977f87edb220cb3a5351c271b
ah-james <11ahj11@gmail.com>
2023-03-08 15:20:34 -0500

    built out binary search insert

--- trees/binarySearchTree.js
+++ trees/binarySearchTree.js
@@ -12,7 +12,30 @@ class BinarySearchTree {
     }
 
     insert(value) {
+        const newNode = new Node(value)
 
+        if (!this.root) {
+            this.root = newNode
+            return this
+        }
+        let tempNode = this.root
+        while (tempNode.left || tempNode.right) { // while the temporary node doesn't have null in either of the left or right values
+            // check if newNode value is greater than tempNode value AND if tempNode.right exists
+            if (value > tempNode.value && tempNode.right) {
+                tempNode = tempNode.right // if value > temNode value and tempNode.right exists, then move tempNode to start from scratch
+            } else if (value < tempNode.value && tempNode.left) { // same as above happens with left, but use less than
+                tempNode = tempNode.left
+            } else {
+                break // if this isn't here it's an infinite loop
+            }
+        }
+
+        // if the current value is greater than the tempNode's value, set tempNode.right to newNode, otherwise it's less and set tempNode.left to newNode
+        if (value > tempNode.value) {
+            tempNode.right = newNode
+        } else {
+            tempNode.left = newNode
+        }
     }
 
     lookup(value) {
@@ -21,3 +44,8 @@ class BinarySearchTree {
 }
 
 const tree = new BinarySearchTree()
+tree.insert(5)
+tree.insert(7)
+tree.insert(1)
+tree.insert(2)
+console.log(tree)
