56d18f6702f4f50ccae1c90e21fc128cb3c64778
ah-james <11ahj11@gmail.com>
2023-03-08 14:32:41 -0500

    framework of binary search tree

---
+++ trees/binarySearchTree.js
@@ -0,0 +1,23 @@
+class Node {
+    constructor(value) {
+        this.right = null
+        this.left = null
+        this.value = value
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+
+    insert(value) {
+
+    }
+
+    lookup(value) {
+        // returns full node or null
+    }
+}
+
+const tree = new BinarySearchTree()
