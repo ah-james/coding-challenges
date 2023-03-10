96418e112110729196cea6a0b3bc1a906306c6d2
ah-james <11ahj11@gmail.com>
2023-03-10 14:08:09 -0500

    adding addVertex method

--- graphs/graph.js
+++ graphs/graph.js
@@ -3,6 +3,18 @@ class Graph {
         this.numberNodes = 0
         this.adjacentList = {}
     }
+
+    addVertex(node) {
+        this.adjacentList[node] = [] // add the node to the adjacentList, this will be filled when an edge is added
+        this.numberNodes++
+    }
+
+    addEdge(node1, node2) {
+
+    }
 }
 
 const myGraph = new Graph()
+myGraph.addVertex(1)
+myGraph.addVertex(2)
+console.log(myGraph)
