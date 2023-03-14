963b7f2d46b8f8eb4042d2d802bca7ab25ab760a
ah-james <11ahj11@gmail.com>
2023-03-14 13:29:58 -0400

    bubble sort working, could be more efficient

---
+++ sortingAlgos/bubbleSort.js
@@ -0,0 +1,31 @@
+// given an array of numbers, create a function to bubble sort them
+
+const bubbleSort = array => {
+    // set count
+    let count = 0
+    let first = 0
+    let second = 1
+
+    while (count <= array.length) {
+        if (array[first] > array[second]) { // if first pointers is higher than second
+            let temp = array[second]
+            array[second] = array[first] // move the first pointer to second pointer position
+            array[first] = temp // move second pointer to first's position
+            first++ // make first and second 1 higher to check next
+            second++ 
+        } else if (array[first] < array[second]) { // if first pointer is already lower than second just increment first and second variables
+            first++
+            second++
+        } else { // reset first and second if we've reached the end of the array
+            count++
+            first = 0
+            second = 1
+        }
+    }
+
+    return array
+}
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+bubbleSort(numbers)
+console.log(numbers)
