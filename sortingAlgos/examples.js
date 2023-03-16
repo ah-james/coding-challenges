b1e1d3b79f6267b5a387a6c381593f0e0026d03e
ah-james <11ahj11@gmail.com>
2023-03-16 15:24:11 -0400

    sorting examples worksheet

---
+++ sortingAlgos/examples.js
@@ -0,0 +1,15 @@
+// Sort 10 schools around your house by distance
+
+// eBay sorts listings by the current Bid amount
+
+// Sort scores on ESPN
+
+// Massive database (can't fit all into memory) needs to sort through past year's user data
+
+// Almost sorted Udemy review data needs to update and add 2 new reviews
+
+// Temperature records for the past 50 years in Canada
+
+// large username database needs to be sorted, data is very random
+
+//You wnat to teach sorting for the first time
