729bb216d59a358dfcdf716a2b49a5bac2fffbc4
ah-james <11ahj11@gmail.com>
2023-03-16 15:41:35 -0400

    sorting answers

--- sortingAlgos/examples.js
+++ sortingAlgos/examples.js
@@ -1,15 +1,23 @@
 // Sort 10 schools around your house by distance
+// insertion sort, very fast & easy to code, low space complexity
 
 // eBay sorts listings by the current Bid amount
+// radix or counting sort, because bids will always be a number within a certain range
 
 // Sort scores on ESPN
+// quick sort, good space complexity with high efficiency
 
 // Massive database (can't fit all into memory) needs to sort through past year's user data
+// merge sort consistent complexity, make sure that we get O(n log n)
 
 // Almost sorted Udemy review data needs to update and add 2 new reviews
+// Insertion sort, already sorted and small amount to be added
 
 // Temperature records for the past 50 years in Canada
+// if temps don't have decimal: radix sort, quick sort if not for in-memory sorting
 
 // large username database needs to be sorted, data is very random
+// merge if memory is expensive, quick sort if not worried about memory
 
 //You wnat to teach sorting for the first time
+// bubble sort, selection sort
