4229de9b17166cdd934c2c12d0eaf4d82726e5dc
ah-james <11ahj11@gmail.com>
2023-03-20 16:51:37 -0400

    climbing stairs function

--- challenges/climbingStairs.js
+++ challenges/climbingStairs.js
@@ -4,9 +4,13 @@
 // ex. n = 3, output 3 (1 + 1 + 1, 1 + 2, 2 + 1)
 
 const climbingStairs = n => {
-    if (n <= 3) {
-        return n
+    if (n < 4) return n
+
+    let fibonacci = [1, 1] // create array, each answer will be pushed into this
+
+    for (let i = 2; i <= n; i++) {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2] // fibonacci element at current step is set equal to the sum of the two preceeding elements ex. fibonacci[3] is equal to 1 + 1
     }
 
-    
+    return fibonacci[n]
 }
