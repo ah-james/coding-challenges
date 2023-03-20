07fa2b8ab7f1d7af6b26cbf2e2a53ad5094cbe44
ah-james <11ahj11@gmail.com>
2023-03-20 13:33:08 -0400

    beginning climbing stairs challenge

---
+++ challenges/climbingStairs.js
@@ -0,0 +1,12 @@
+// You are climbing a staircase. It takes n steps to reach the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+// ex. Input n = 2, output 2 (1 + 1 or 2)
+// ex. n = 3, output 3 (1 + 1 + 1, 1 + 2, 2 + 1)
+
+const climbingStairs = n => {
+    if (n <= 3) {
+        return n
+    }
+
+    
+}
