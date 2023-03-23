a945f7b01c8ed6889073a5a42b9dbb5e0e39d65d
ah-james <11ahj11@gmail.com>
2023-03-23 11:50:36 -0400

    starting work on singleNumber

---
+++ challenges/singleNumber.js
@@ -0,0 +1,13 @@
+// given an array return the single number
+
+const singleNumber = array => {
+    const obj = {}
+
+    for (let i = 0; i < array.length; i++) {
+        if (!obj[array[i]]) {
+            obj[array[i]] = 1
+        } else {
+            
+        }
+    }
+}
