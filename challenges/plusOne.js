2add8f84851b2a847ddfd04d1e7565af9e1ca446
ah-james <11ahj11@gmail.com>
2023-03-28 12:41:51 -0400

    continuing work on plusOne

--- challenges/plusOne.js
+++ challenges/plusOne.js
@@ -1,5 +1,16 @@
 const plusOne = digits => {
-    let num = digits.join('')
-    let returnedNum = Number(num) + 1
-    return Array.from(String(returnedNum), number => Number(number))
+    // let num = digits.join('')
+    // let returnedNum = Number(num) + 1
+    // return Array.from(String(returnedNum), number => Number(number))
+
+    let length = digits.length
+    if (digits[length - 1] === 9) {
+        digits.pop()
+        digits.push(1)
+        digits.push(0)
+        return digits
+    }
+    
+    digits[length - 1] = digits[length - 1] + 1
+    return digits
 }
