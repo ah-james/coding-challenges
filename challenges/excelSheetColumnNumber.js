299763fa10d2e0ff61fbb92316b1c0f9b1df292d
ah-james <11ahj11@gmail.com>
2023-04-01 12:01:08 -0400

    adding explanation to titleToNumber

--- challenges/excelSheetColumnNumber.js
+++ challenges/excelSheetColumnNumber.js
@@ -6,7 +6,9 @@ const titleToNumber = columnTitle => {
     for (let i = 0; i < columnTitle.length; i++) {
         const currentLetter = columnTitle[i] // current Letter
         const currentNumber = currentLetter.charCodeAt() - 64 // corresponding number to letter, subtract 64 because 65 is beginning of capital letters in ASCII
-        returnedNumber = currentNumber * Math.pow(26, columnTitle.length - i - 1)
+        returnedNumber += currentNumber * Math.pow(26, columnTitle.length - i - 1) // first number to the 2nd number power, so if columnTitle length is 2, second number would be 
+        // 2-0-1=1 for the first iteration, so if currentletter is B, full equation is 2 * 26^1 = 52
+        // then use += so that next iteration is added to the first
     }
     return returnedNumber
 }
