0392ab7c5769c6b782a03cef005f908b3cb3f1bb
ah-james <11ahj11@gmail.com>
2023-06-06 15:28:12 -0400

    maxChar challenge completed

---
+++ challenges/maxCharacter.js
@@ -0,0 +1,21 @@
+// Given a string, return the character that is most commonly used in the string.
+// ex maxChar("abcccccccd") === "c"
+
+const maxChar = str => {
+    let obj = {}
+    let maxValue
+    let returnedCharacter
+
+    for (const char of str) {
+        obj[char] = obj[char] + 1 || 1
+    }
+
+    for (const char in obj) {
+        if (obj[char] > maxValue) {
+            maxValue = obj[char]
+            returnedCharacter = char
+        }
+    }
+
+    return returnedCharacter
+}
