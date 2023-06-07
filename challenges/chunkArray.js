81cb509941aa72975a3bc2f7d2671920d26856c6
ah-james <11ahj11@gmail.com>
2023-06-07 17:21:48 -0400

    adding second solution to chunk challenge

--- challenges/chunkArray.js
+++ challenges/chunkArray.js
@@ -6,13 +6,33 @@ const chunk = (array, size) => {
     const chunkedArray = []
 
     for (let element of array) {
+        // is there an element in chunkedArray
         const last = chunkedArray[chunkedArray.length - 1]
-        // if last element in chunked doesn't exist or is equal to size
+        // if last element in chunked doesn't exist or is equal to size of each chunk
         if (!last || last.length === size) {
+            // push a new array into chunked of current element
             chunkedArray.push([element])
         } else {
+            // if a previous iteration created a new array in chunked, then push current element into that last array
             last.push(element)
         }
+    }
+
+    return chunkedArray
+}
+
+const chunkTwo = (array, size) => {
+    // empty array to hold chunks
+    const chunkedArray = []
+    let i = 0
+
+    // while loop, when i is less than array length
+    while (i < array.length) {
+        // slice off chunk of array, starting at i, ending at i + required size of the chunk 
+        const slice = array.slice(i, i + size)
+        chunkedArray.push(slice)
+        // i needs to be increased by size to make sure it doesn't duplicate elements
+        i += size
     }
 
     return chunkedArray
