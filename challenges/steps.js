9139c8fb06e29e13dbbe7739b723470e9eb5a9fb
ah-james <11ahj11@gmail.com>
2023-06-08 09:44:25 -0400

    steps challenge completed

---
+++ challenges/steps.js
@@ -0,0 +1,29 @@
+// Write a function that accepts a positive number N. 
+// The function should console log a step shape with N levels using the # character.  
+// Make sure the step has spaces on the right hand side!
+// ex. steps(3);
+//    '#  '
+//    '## '
+//    '###'
+
+const steps = n => {
+    //   create string variable to log and variable to count length of string
+    let string = ''
+    let length = 0
+
+    // loop
+    for (let i = 1; i <= n; i++) {
+        // add # until string length is equal to the loop iteration
+        while (string.length < i) {
+            string += '#'
+        }
+        // add whitespace until string length is equal to n
+        while (string.length < n) {
+            string += ' '
+        }
+
+        console.log(string)
+        // reset string length for next loop
+        string = ''
+    }
+}
